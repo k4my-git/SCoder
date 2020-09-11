774c3d879889ea661d6b0cfb3415601718b53aa6
vagrant <vagrant@localhost.localdomain>
2020-09-11 19:02:01 +0000

    update-top

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,9 +18,32 @@
 //= require_tree .
 
 window.onload = function() {
-  Particles.init({
-  selector: '.background',
-  sizeVariations: 50,
-  color: ['#00bbdd', '#404B69', '#DBEDF3'],
-  connectParticles: true
+	Particles.init({
+	selector: '.background',
+	sizeVariations: 50,
+	color: ['#00bbdd', '#404B69', '#DBEDF3'],
+	connectParticles: true
+	responsive: [
+	    {
+	      	breakpoint: 768,
+			options: {
+			    maxParticles: 200,
+			    color: '#48F2E3',
+			    connectParticles: false
+			}
+		},
+		{
+			breakpoint: 425,
+			options: {
+			    maxParticles: 100,
+			    connectParticles: true
+			}
+		}, 
+		{
+			breakpoint: 320,
+			options: {
+			    maxParticles: 0// disables particles.js
+	    	}
+	    }
+	]
 })};
