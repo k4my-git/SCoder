fdd7364a8415296a4c7590f8af7d70547d788e8a
vagrant <vagrant@localhost.localdomain>
2020-09-13 14:58:55 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about', as:'homes_about'
 
   devise_for :users
+  
   resources :users, only: [:show, :edit, :update]
   resources :codes, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
   	resource :favorites, only: [:create, :destroy]
